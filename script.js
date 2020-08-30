b2026568d9072c5d3d27a20b9763bd7f797fe6bb
Jones Navela <NavelaJones@gmail.com>
2020-08-30 11:27:05 +0200

    set Timeout counter

--- script.js
+++ script.js
@@ -11,7 +11,16 @@ function breathAnimation() {
   text.innerText = 'Breathe In!';
   container.className = 'container grow';
 
-  
+  setTimeout(() => {
+    text.innerText = 'Hold';
+
+    setTimeout(() => {
+      text.innerText = 'Breathe Out!';
+      container.className = 'container shrink';
+    }, holdTime);
+  }, breatheTime);
+}
+
 }
 
 setInterval(breathAnimation, totalTime);
